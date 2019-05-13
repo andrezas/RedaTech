de39c6c66fae17992e4fb76d161b9b72719ce42b
Aluno <Aluno@DESKTOP-3FIS8IH>
2019-05-13 08:50:02 -0300

    13/05/19 - Mudanças de entidades

---
+++ RedaTech/src/Comentario.java
@@ -0,0 +1,10 @@
+import java.util.Calendar;
+
+public class Comentario {
+	
+	private long id;
+	private Calendar data;
+	private Usuario usuario;
+	
+
+}
---
+++ RedaTech/src/Usuario.java
@@ -0,0 +1,115 @@
+import java.util.Calendar;
+
+public class Usuario {
+	private Long id;
+	private String email;
+	private String nome;
+	private Calendar nascimento;
+	private String nomeUsuário;
+	private String senha;
+	private String grupo;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Calendar getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(Calendar nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public String getNomeUsuário() {
+		return nomeUsuário;
+	}
+	public void setNomeUsuário(String nomeUsuário) {
+		this.nomeUsuário = nomeUsuário;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getGrupo() {
+		return grupo;
+	}
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((grupo == null) ? 0 : grupo.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nascimento == null) ? 0 : nascimento.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((nomeUsuário == null) ? 0 : nomeUsuário.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (grupo == null) {
+			if (other.grupo != null)
+				return false;
+		} else if (!grupo.equals(other.grupo))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nascimento == null) {
+			if (other.nascimento != null)
+				return false;
+		} else if (!nascimento.equals(other.nascimento))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (nomeUsuário == null) {
+			if (other.nomeUsuário != null)
+				return false;
+		} else if (!nomeUsuário.equals(other.nomeUsuário))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		return true;
+	}
+	
+}
